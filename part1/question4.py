8c91834fd36f2d720f3eb6edbe8920b30b352877
Angie Castano <angieximena98@gmail.com>
2023-11-04 18:32:09 -0500

    [ADD][SQL commands]

--- part1/question4.py
+++ part1/question4.py
@@ -23,9 +23,7 @@ import pets_db
 # The output should be a list of tuples in the format: (<pet name>, <species>, <age>)
 
 sql_pets_owned_by_nobody = """
-
-Your SQL here.
-
+SELECT name, species, age FROM animals WHERE animal_id NOT IN (select pet_id FROM People_animals);
 """
 
 # Part 4.B:
@@ -34,7 +32,7 @@ Your SQL here.
 
 sql_pets_older_than_owner = """
 
-Your SQL here.
+SELECT count (age) FROM (SELECT people.person_id, people.age as age_p, people_animals.pet_id FROM people JOIN people_animals ON people_animals.owner_id = people.person_id) JOIN animals ON animals.animal_id = pet_id WHERE age > age_p;
 
 """
 
@@ -43,6 +41,6 @@ Your SQL here.
 # The output should be a list of tuples in the format: (<person name>, <pet name>, <species>)
 sql_only_owned_by_bessie = """ 
 
-Your SQL here.
+SELECT p_name, name, species FROM (SELECT people.name AS p_name, people.person_id, people.age as age_p, people_animals.pet_id FROM people JOIN people_animals ON people_animals.owner_id = people.person_id) JOIN animals ON animals.animal_id = pet_id WHERE person_id = (SELECT person_id FROM people WHERE name = "bessie") AND pet_id NOT IN (SELECT pet_id FROM (SELECT people.name AS p_name, people.person_id, people.age as age_p, people_animals.pet_id FROM people JOIN people_animals ON people_animals.owner_id = people.person_id) JOIN animals ON animals.animal_id = pet_id WHERE person_id != (SELECT person_id FROM people WHERE name = "bessie"))
 
 """
