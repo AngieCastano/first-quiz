9e10a8cc93cd92aca155f5a35f4d2ab62fab05b7
Angie Castano <angieximena98@gmail.com>
2023-11-05 09:19:30 -0500

    [ADD][Main class, modified class VendingMachine, Drink]

--- part2/src/main/java/org/velezreyes/quiz/question6/Drink.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/Drink.java
@@ -1,7 +1,25 @@
 package org.velezreyes.quiz.question6;
 
-public interface Drink {
-  public String getName();
+public class Drink {
+  String name;
+  boolean fizzy;
+  public Drink(String name, boolean fizzy) {
+        this.name = name;
+        this.fizzy = fizzy;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public boolean isFizzy();
+    public void setFizzy(boolean fizzy) {
+        this.fizzy = fizzy;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+  public boolean isFizzy(){
+      return this.fizzy;
+  }
 }
--- part2/src/main/java/org/velezreyes/quiz/question6/VendingMachine.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/VendingMachine.java
@@ -1,8 +1,40 @@
 package org.velezreyes.quiz.question6;
 
-public interface VendingMachine {
+public class VendingMachine{
+  
+    public int Quarter = 0;
+  
+ 
 
-  public void insertQuarter();
+  public void insertQuarter(){
+      Quarter++;
+  }
 
-  public Drink pressButton(String name) throws NotEnoughMoneyException, UnknownDrinkException;
+  public Drink pressButton(String name) throws NotEnoughMoneyException, UnknownDrinkException{
+      Drink drink = null;
+      if ("ScottCola".equals(name) ){
+          if(Quarter >= 3){
+              drink = new Drink(name, true);
+             Quarter = 0; 
+          }
+          else{
+              throw new NotEnoughMoneyException();
+          }
+      }
+      else if ("KarenTea".equals(name)){
+          if(Quarter >= 4){
+              drink = new Drink(name, false);
+              Quarter = 0;
+          }
+          else{
+              throw new NotEnoughMoneyException();
+          }
+      }
+      else{
+          throw new UnknownDrinkException();
+      }
+      return drink;
+      
+      
+  }
 }
